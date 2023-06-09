9e4ac13b212eaf95cf094bca6af34b2b5ab736ca
tariq hadi <tariqhadi@hotmail.com>
2023-06-09 23:03:59 +0300

    after some changes

--- TariqTalat,DrHamada,Task.R
+++ TariqTalat,DrHamada,Task.R
@@ -1,81 +1,87 @@
-# First, install and load the required packages
-install.packages("readr")
-install.packages("dplyr")
 install.packages("ggplot2")
+install.packages("dblyr")
+install.packages("readr")
 
-library(readr)
-library(dplyr)
+
+# First, import the required libraries
 library(ggplot2)
+library(dplyr)
+library(readr)
+
 
 # Set the file path
-filepath <- file.path(getwd(), "G3_sydney_hobart_times.csv")
+df <- read.csv("G3_sydney_hobart_times.csv")
 
-# Load the dataset
-df <- read_csv(filepath)
 
-# Getting the first 5 values
+# Display the first 5 values
 head(df)
 
-# Getting the last 5 values
+
+# Display the last 5 values
 tail(df)
 
-# Getting the shape of the values
+
+# Check the shape of the dataset
 dim(df)
+ 
 
-# Checking for NULL values
+# Check for NULL values
 sum(is.na(df))
+#We Have 79 NuLL Values
 
-# Descriptive statistics about the values
+
+
+# Descriptive statistics of the dataset
 summary(df)
 
-# Dropping the NULL column
-df <- df %>% select(-`Code Time less than 3`)
+
 
 # Split the values in the 'Time' column and keep only the numbers
-df$Time <- sapply(strsplit(df$Time, " "), "[", 1)
+df$Time <- as.numeric(gsub("[^0-9.]+", "", df$Time))
 
-# Convert the values to numeric type
-df$Time <- as.numeric(df$Time)
 
-# Checking the 'Time' column again
-length(df$Time)
+
+# Check the 'Time' column again
 head(df$Time)
 sum(is.na(df$Time))
+#7 Null Values
+#Looking At the Dataset and the Std using Mean would be the best fit
+
+
+
 
 # Calculate the mean of the 'Time' column
 mean_time <- mean(df$Time, na.rm = TRUE)
-
-# Fill the missing values in the 'Time' column with the mean value
+# Fill missing values in the 'Time' column with the mean
 df$Time[is.na(df$Time)] <- mean_time
 
-# One last check
+
+
+
+# Check for missing values in the 'Time' column again
 sum(is.na(df$Time))
-round(df$Time, 2)
+df$Time <- round(df$Time, 2)
 
-# Asking and Analysis phase
 
-# Calculate the difference between 'fleet_start' and 'fleet_finish' to get the completion time
-df$Completed_the_race <- df$fleet_start - df$fleet_finish
 
-# Sort the DataFrame by the completion time
-sorted_df <- df[order(df$Completed_the_race), ]
+# Perform calculations on "Code Time less than 3"
+df$`Time less than 3` <- df$Time < 3
 
-# Get the row with the minimum completion time
-min_row <- sorted_df[1, ]
+df$`Completed the race` <- df$fleet_start - df$fleet_finish
+# Analysis phase
 
-# Get the year and the least value
+# Find the year and value with the least 'Completed the race'
+min_row <- df[which.min(df$`Completed the race`), ]
 min_year <- min_row$Year
-min_value <- min_row$Completed_the_race
+min_value <- min_row$`Completed the race`
 
-# Get the row with the maximum completion time
-max_row <- sorted_df[nrow(sorted_df), ]
-
-# Get the year and the maximum value
+# Find the year and value with the highest 'Completed the race'
+max_row <- df[which.max(df$`Completed the race`), ]
 max_year <- max_row$Year
-max_value <- max_row$Completed_the_race
+max_value <- max_row$`Completed the race`
 
-# Create a scatter plot to visualize the completion times
-plt <- ggplot(df, aes(x = Year, y = Completed_the_race)) +
+# Scatter plot of 'Year' vs 'Completed the race'
+plt <- ggplot(df, aes(x = Year, y = `Completed the race`)) +
   geom_point() +
   geom_point(data = min_row, aes(x = min_year, y = min_value), color = "red", show.legend = FALSE) +
   geom_point(data = max_row, aes(x = max_year, y = max_value), color = "green", show.legend = FALSE) +
@@ -84,42 +90,50 @@ plt <- ggplot(df, aes(x = Year, y = Completed_the_race)) +
   labs(x = "Year", y = "Completed the race", title = "The difference between the Start And End") +
   theme_bw()
 
-print(plt)
 
-# Group the data by "Year" and calculate the total fleet starts for each year
-fleet_starts <- df %>% group_by(Year) %>% summarize(total_fleet_starts = sum(fleet_start))
+
+# Group the data by 'Year' and calculate the total fleet starts for each year
+fleet_starts <- df %>% group_by(Year) %>% summarise(total_fleet_starts = sum(fleet_start))
+
 
 # Sort the fleet starts in ascending order
 sorted_fleet_starts <- fleet_starts[order(fleet_starts$total_fleet_starts), ]
 
-# Get the years with the most fleet starts in ascending order
-years_most_fleet_starts <- sorted_fleet_starts$Year
-
 # Create a bar plot to visualize the years with the most fleet starts
-plt <- ggplot(sorted_fleet_starts, aes(x = as.factor(Year), y = total_fleet_starts)) +
+ggplot(sorted_fleet_starts, aes(x = Year, y = total_fleet_starts)) +
   geom_bar(stat = "identity") +
-  labs(x = "Year", y = "Fleet Starts", title = "Years with the Most Fleet Starts (Ascending Order)") +
-  theme_bw() +
+  labs(x = "Year", y = "Fleet Starts") +
+  ggtitle("Years with the Most Fleet Starts (Ascending Order)") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-print(plt)
 
 # Find the maximum number of fleet starts
 max_fleet_starts <- max(df$fleet_start)
 
 # Filter the data to get the year(s) with the maximum number of fleet starts
-years_with_max_fleet_starts <- df[df$fleet_start == max_fleet_starts, "Year"]
+years_with_max_fleet_starts <- df$Year[df$fleet_start == max_fleet_starts]
 
 # Print the year(s) with the maximum number of fleet starts
 cat("Year(s) with the maximum number of fleet starts:", years_with_max_fleet_starts, "with", max_fleet_starts, "\n")
 
-# Group the DataFrame by "Time" and count the number of occurrences of each "Time" value
-time_counts <- df %>% group_by(Time) %>% summarize(fleet_finish_count = n())
+# Count the occurrences of each 'Time' value
+time_counts <- count(df, Time)
 
 # Create a bar chart to visualize the number of fleet finishes by time
-plt <- ggplot(time_counts, aes(x = Time, y = fleet_finish_count)) +
+ggplot(time_counts, aes(x = Time, y = n)) +
   geom_bar(stat = "identity") +
-  labs(x = "Time", y = "Number of fleet finishes", title = "Number of Fleet Finishes by Time") +
-  theme_bw()
+  labs(x = "Time", y = "Number of fleet finishes") +
+  ggtitle("Number of Fleet Finishes by Time")
 
-print(plt)
+# Create a new column 'Time less than 3' with 'More than 3' and 'Less than 3' values
+df$`Time less than 3` <- ifelse(df$Time < 3, "Less than 3", "More than 3")
+
+# Count the occurrences of 'More than 3' and 'Less than 3' in 'Time less than 3' column
+time_less_than_3_counts <- count(df, `Time less than 3`)
+
+# Create a pie chart to visualize the counts
+ggplot(time_less_than_3_counts, aes(x = "", y = n, fill = `Time less than 3`)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y") +
+  labs(fill = "Time less than 3") +
+  ggtitle("Time less than 3") +
+  theme_void()
